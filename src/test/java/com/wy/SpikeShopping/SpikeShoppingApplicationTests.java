32bdea1df73ba09c3a3f86fd04443931f08804da
yongwang <ikwhere@gmail.com>
2023-02-03 23:44:31 -0500

    Nothing but a framework

---
+++ src/test/java/com/wy/SpikeShopping/SpikeShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wy.SpikeShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpikeShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
